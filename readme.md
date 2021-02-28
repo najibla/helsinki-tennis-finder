f3285db2b756aeb49f23b3b731a0e49f987e1d44
Mohamed Najib Laaziz <m.najib.laaziz@gmail.com>
2021-02-28 14:01:43 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Helsinki Tennis Finder
+
+helsinki-tennis-finder shows available slots in Smash, Tali and Puhos within 15 days.
+
+## Motive?
+
+It's very hard to find tennis slots in Helsinki these days. People book way in advance, but it happens that they cancel during the same day or a day before.
+
+## Current state
+
+Currently the program shows only available slots from Tuesday to Thursday and from 5PM to 7:30PM
+
+## How to run
+
+Assuming you have GIT, Node and Yarn/NPM installed
+
+- Clone source code.
+- Run `yarn install` in project root
+- Run `node src/index.js` in project root
+
+## Future development
+
+- Make it more configurable (select time range, select days)
+- Add notification service
+
+## License [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+
+This software is released under the [MIT license](../blob/master/LICENSE)
+ 
---
+++ src/index.js
@@ -0,0 +1,92 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const jquery = require('jquery');
+const { JSDOM } = require('jsdom');
+const util = require('util');
+
+const centers = ['smash', 'talin', 'puhos'];
+
+const links = {
+	smash: 'https://smashcenter.slsystems.fi',
+	talin: 'https://varaukset.talintenniskeskus.fi',
+	puhos: 'https://puhoscenter.slsystems.fi',
+};
+
+const allResults = {
+	smash: [],
+	talin: [],
+	puhos: [],
+};
+
+async function getData(centerName, date) {
+	const resp = await fetch(
+		`${links[centerName]}/booking/booking-calendar?BookingCalForm%5Bp_pvm%5D=${date}`
+	).catch(`Coulnt get data for ${centerName} in date ${date}`);
+
+	return await resp.text();
+}
+
+function parseResults(data) {
+	const results = [];
+	const result = new JSDOM(data);
+	const $ = jquery(result.window);
+	// get only 60 min available and 30 min available
+	$('.s-avail a, .s-avail-short a').each((_i, val) => {
+		const params = new URLSearchParams($(val).attr('href').split('?')[1]);
+		// this is needed because Smash shows also PALLOTYKKI availabilities
+		const courtNo = params.get('resid');
+		const startTime = params.get('alkuaika');
+		const startingHour = `${moment(startTime).hours()}${moment(
+			startTime
+		).minute()}`;
+		const isGoodTime = startingHour > '165' && startingHour < '194';
+		if (Number(courtNo) < 30 && isGoodTime) {
+			results.push({
+				time: startTime,
+				duration: params.get('kesto'),
+				court: courtNo,
+			});
+		}
+	});
+	return results;
+}
+
+function sortResults(result) {
+	for (const [key, value] of Object.entries(result)) {
+		result[key] = value.sort((a, b) => a.date.localeCompare(b.date));
+	}
+	return result;
+}
+
+async function main() {
+	await Promise.all(
+		// check within 15 days
+		Array.from(Array(15).keys()).map(async (i) => {
+			const today = moment();
+			const theDate = today.add(i, 'days');
+			const dayOfWeek = theDate.weekday();
+			// check only tuesday => Thursday
+			if (dayOfWeek > 1 && dayOfWeek < 5) {
+				const formattedToday = theDate.format('YYYY-MM-DD');
+				await Promise.all(
+					centers.map(async (center) => {
+						const data = await getData(center, formattedToday);
+						const parseData = parseResults(data);
+                        if (parseData.length) {
+                            allResults[center].push({
+                                date: formattedToday,
+                                availablitites: parseData,
+                            });
+                        }
+						return Promise.resolve();
+					})
+				);
+			}
+		})
+	);
+	return sortResults(allResults);
+}
+
+main()
+	.then((val) => console.log(util.inspect(val, false, null, true)))
+	.catch((error) => console.log(util.inspect(error, false, null, true)));
